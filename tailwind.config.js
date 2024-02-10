c29e5653b8a8aeae462fa246491d3ec34326170e
kqbusiness <159662091+kqbusiness@users.noreply.github.com>
2024-02-11 00:25:56 +0100

    Made input.css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Guerilla: ["Roboto Condensed", "sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
